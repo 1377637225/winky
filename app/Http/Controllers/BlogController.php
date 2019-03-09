f83e118f44e32ae65647a3ce507a6145cb815cfd
“1377637225 <1377637225@qq.com>
2019-03-09 19:53:25 +0800

    ..

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Wink\WinkPost;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        
+        $posts = WinkPost::with('tags')
+            ->live()
+            ->orderBy('publish_date', 'DESC')
+            ->simplePaginate(12);
+
+            return view('blog.index', [
+                'posts' => $posts,
+            ]);
+    }
+
+    public function show($slug)
+    {
+        $post = WinkPost::live()->whereSlug($slug)->firstOrFail();
+        return view('post.index', [
+            'post' => $post,
+        ]);
+    }
+}
